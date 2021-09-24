df14025dd6e35556111602248bb0393c9ff1c50f
sagittarius3008 <sagittarius30008@gmail.com>
2021-09-24 08:14:12 +0000

    chapter1

---
+++ main.java
@@ -0,0 +1,20 @@
+public class main{
+    public static void main(String[] args){
+        final double TAX = 1.08;
+        int fax = 5;
+        System.out.println("5から値下げっす");
+        fax = 4;
+        System.out.println(fax * TAX + "万円");
+        // Q1
+        int height = 3;
+        int wight = 5;
+        System.out.println(height * wight);
+        
+        boolean result = true;
+        char favoriteCharacter = '駆';
+        double pi = 3.14;
+        long number = 31415;
+        String msg = "ミナトの攻撃！　15ポイントのダメージ";
+        System.out.println(msg);
+    }
+}

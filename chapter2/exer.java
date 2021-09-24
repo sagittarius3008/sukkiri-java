8e9c8e07f5041e8dc8816474b3be4ff2731c8c71
sagittarius3008 <sagittarius30008@gmail.com>
2021-09-24 14:17:25 +0000

    chapter2

---
+++ chapter2/exer.java
@@ -0,0 +1,38 @@
+public class exer{
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        String ans = "あなたは" + (x + y);
+        System.out.println(ans);
+        double b = 9/2.0;
+        // System.out.println(b);
+        float c = 9/3.0F;
+        // System.out.println(c);
+        b++;
+        // System.out.println(b);
+        // int age = (int)3.2; // 強制的な型変換
+        // System.out.println(age);
+        String name = "赤子";
+        
+        // System.out.printは改行されない
+        // System.out.print(name);
+        // System.out.print(age + "歳");
+        
+        // Math.max：大きい方の数字を代入する
+        // int m = Math.min(x,a);
+        // System.out.println(m);
+        
+        // 文字列を数字に変換
+        // String aging = "31";
+        // int n = Integer.parseInt(aging);
+        // System.out.println(n);
+        
+        // nextInt(XX)までの値をランダムで生成
+        // int r = new java.util.Random().nextInt(90);
+        // System.out.println(r);
+        // System.out.println("名前と年齢を入力してくれ");
+        // String nam = new java.util.Scanner(System.in).nextLine();
+        // int age = new java.util.Scanner(System.in).nextInt();
+        // System.out.println(age + "の" + nam);
+    }
+}
---
+++ chapter2/main.java
@@ -0,0 +1,39 @@
+public class main{
+    public static void main(String[] args){
+        int x = 0x0010;
+        // System.out.println(x);
+        // System.out.println("二重引用符(\")です");
+        int a = 9/2;
+        // System.out.println(a);
+        double b = 9/2.0;
+        // System.out.println(b);
+        float c = 9/3.0F;
+        // System.out.println(c);
+        b++;
+        // System.out.println(b);
+        // int age = (int)3.2; // 強制的な型変換
+        // System.out.println(age);
+        String name = "赤子";
+        
+        // System.out.printは改行されない
+        // System.out.print(name);
+        // System.out.print(age + "歳");
+        
+        // Math.max：大きい方の数字を代入する
+        // int m = Math.min(x,a);
+        // System.out.println(m);
+        
+        // 文字列を数字に変換
+        // String aging = "31";
+        // int n = Integer.parseInt(aging);
+        // System.out.println(n);
+        
+        // nextInt(XX)までの値をランダムで生成
+        // int r = new java.util.Random().nextInt(90);
+        // System.out.println(r);
+        System.out.println("名前と年齢を入力してくれ");
+        String nam = new java.util.Scanner(System.in).nextLine();
+        int age = new java.util.Scanner(System.in).nextInt();
+        System.out.println(age + "の" + nam);
+    }
+}

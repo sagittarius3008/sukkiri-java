a71741ea745e390453e13b7af6afd97323960346
sagittarius3008 <sagittarius30008@gmail.com>
2021-09-26 05:14:03 +0000

    chapter2

--- chapter2/exer.java
+++ chapter2/exer.java
@@ -1,38 +1,21 @@
 public class exer{
     public static void main(String[] args){
-        int x = 5;
-        int y = 10;
-        String ans = "あなたは" + (x + y);
-        System.out.println(ans);
-        double b = 9/2.0;
-        // System.out.println(b);
-        float c = 9/3.0F;
-        // System.out.println(c);
-        b++;
-        // System.out.println(b);
-        // int age = (int)3.2; // 強制的な型変換
-        // System.out.println(age);
-        String name = "赤子";
+        // int x = 5;
+        // int y = 10;
+        // String ans = "あなたは" + (x + y);
+        // System.out.println(ans);
         
-        // System.out.printは改行されない
-        // System.out.print(name);
-        // System.out.print(age + "歳");
-        
-        // Math.max：大きい方の数字を代入する
-        // int m = Math.min(x,a);
-        // System.out.println(m);
-        
-        // 文字列を数字に変換
-        // String aging = "31";
-        // int n = Integer.parseInt(aging);
-        // System.out.println(n);
-        
-        // nextInt(XX)までの値をランダムで生成
-        // int r = new java.util.Random().nextInt(90);
-        // System.out.println(r);
-        // System.out.println("名前と年齢を入力してくれ");
-        // String nam = new java.util.Scanner(System.in).nextLine();
-        // int age = new java.util.Scanner(System.in).nextInt();
-        // System.out.println(age + "の" + nam);
+        System.out.println("ようこそ占いの館へ");
+        System.out.println("あなたの名前を入力してください");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("あなたの年齢を入力してください");
+        String ageString = new java.util.Scanner(System.in).nextLine();
+        int age = Integer.parseInt(ageString);
+        int fortune = new java.util.Random().nextInt(4);
+        // 結果を1プラスして1~4に
+        fortune++;
+        System.out.println("占いの結果がでました！");
+        System.out.println(age +"歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+        System.out.println("1:大吉・・・");
     }
 }

f361b10a934c20ca3b43c1a0cbd9c11fca274e4f
sagittarius3008 <sagittarius30008@gmail.com>
2021-09-28 14:22:04 +0000

    chapter5

---
+++ chapter5/main.java
@@ -0,0 +1,35 @@
+public class Main{
+    // public static void main(String[] args){
+    //     System.out.println("これからメソッドを呼びます。");
+    //     hello(); //メソッド呼び出し
+    //     System.out.println("呼び出し完了");
+    // }
+    // // helloメソッド
+    // public static void hello(){
+    //     System.out.println("こんにちは");
+    // }
+    
+    // 引数の取り方
+    // public static void main(String[] args){
+    //     System.out.println("これからメソッドを呼びます。");
+    //     hello("ミナト");
+    //     hello("朝香");
+    //     hello("菅原");
+    //     System.out.println("メソッド呼び出し完了");
+    // }
+    // // hello()にて引数の型の宣言
+    // public static void hello(String name){
+    //     System.out.println(name + "さん、はろー");
+    // }
+    
+    // 引数が複数
+    // public static void main(String[] args){
+    //     add(100,20);
+    //     add(200,50);
+    // }
+    // // add()にて複数の値を受け取る
+    // public static void add(int x, int y){
+    //     int ans = x + y;
+    //     System.out.println(x + "+" + y + "=" + ans);
+    // }
+}
